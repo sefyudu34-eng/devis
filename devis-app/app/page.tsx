e0cb9324e31846ce9ea8d3e04ce3c77b203951a4
sefyudu34-eng <sefyudu34@gmail.com>
2025-11-08 22:11:51 +0100

    Structure Next.js avec composants

---
+++ devis-app/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from 'react';
+import AppDevisIA from '@/components/AppDevisIA';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <AppDevisIA />
+    </main>
+  );
+}
